3fd924a23e3ec9a6906982fd38110bb750393a78
Michael Huang <michaelghuang19@gmail.com>
2021-10-11 08:56:14 -0700

    initial 13 city view

--- a1/src/const.py
+++ a1/src/const.py
@@ -3,8 +3,9 @@
 
 import os
 
-home_dir_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+HOME_DIR_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-data_path = home_dir_path + '/data/'
-results_path = home_dir_path + '/results/'
+DATA_PATH = HOME_DIR_PATH + '/data/'
+RESULTS_PATH = HOME_DIR_PATH + '/results/'
 
+SUMMER_LEN = 3
--- a1/src/main.py
+++ a1/src/main.py
@@ -1,9 +1,30 @@
 # A1
 
 import const
+import json
 
 def main():
-  print("Hello world")
+  file = open(const.DATA_PATH + "sunshine.json")
+  json_data = json.load(file)["data"]
+
+  print(json_data.keys())
+
+  # for data in 
+
+# get the x contiguous months with most sun 
+def _kadane(data, count=const.SUMMER_LEN):
+  max_total = 0
+
+  cur_start = 0
+  cur_total = 0
+
+  while cur_start + count <= len(data):
+    cur_total = sum(data[cur_start : cur_start + count])
+    if cur_total > max_total:
+      max_start = cur_start
+      max_total = cur_total
+
+  return max_total, max_start
 
 if __name__ == "__main__":
   main()
